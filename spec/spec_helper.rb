384b350b39c1bde7cc5449f6096f1bb508a740a2
Sivachel <lsivachelvan@spartaglobal.com>
2018-09-24 16:40:48 +0100

    nginx proxy.conf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+# checks for coverage of the text
+at_exit { ChefSpec::Coverage.report! }
